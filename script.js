844ec7af827f99d9fcdf0c605417df649a2c07fa
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-09-23 11:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+const input=document.getElementById("num");
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const obj = [
+      ['M',1000], 
+      ['D', 500], 
+      ['C', 100], 
+      ['L', 50], 
+      ['X', 10], 
+      ['V', 5], 
+      ['I', 1]
+    ]
 
   //your code here
   
@@ -21,14 +22,30 @@ function convertToRoman(num) {
   }
 
   return romanNumeral;
-
-
-
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// let to=document.getElementById("to").addEventListener("click",convertToRoman(input));
+console.log(btn)
+// let output=document.getElementById("roman").appendchild;
 
-// console.log(convertToRoman(36));
 
+
+// do not edit below this line
+// module.exports = convertToRoman
+const inputz=document.getElementById("btn");
+inputz.addEventListener("click",function(){
+    const gotInput=input.value
+     console.log(gotInput)
+     const output1 = convertToRoman(gotInput); // Output will be "XIV"
+
+console.log(output1);
+let container=document.getElementsByClassName("container");
+
+let roman=document.getElementById("roman")
+roman.innerText=output1
+
+})
 
 
 
